ab6a015678648c4571f3606046f0fc0c8626dbcc
Rémi Viez <remiviez@gmail.com>
2025-10-08 02:43:11 +0200

    correction port lancement brut

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  // Charger les variables .env du dossier racine (../) au lieu du dossier frontend
+  const env = loadEnv(mode, path.resolve(__dirname, ".."), "");
 
   return {
     plugins: [react()],
     server: {
       port: parseInt(env.FRONTEND_PORT) || 3000,
-      strictPort: true, // Stoppe le serveur si le port est déjà utilisé
+      strictPort: true,
       open: true,
     },
   };
